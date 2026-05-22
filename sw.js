926667450a209dbfce6425c921b0dadf5431f1cb
Vastify <138656068+Vastify@users.noreply.github.com>
2026-05-22 19:32:09 +1000

    Final Update
    Added a service worker, tested the PWA. Works well and passes the test when on inspect element (127.0.0.1.har is the test downloaded)

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "rate-my-rhythm-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./about.html",
+  "./account.html",
+  "./artists.html",
+  "./genres.html",
+  "./submit_review.html",
+  "./top_rated.html",
+
+  "./style.css",
+
+  "./app.js",
+  "./account.js",
+
+  "./logo.jpg"
+];
+
+/* INSTALL */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+/* ACTIVATE */
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+/* FETCH */
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
